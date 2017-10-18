f557c245f7c12c1b69cba6ea0303506ce46c5f02
Harald Heitmann <harald.heitmann.f@outlook.com>
2017-10-18 12:20:53 -0300

    add new function

--- dirty/utilities.py
+++ dirty/utilities.py
@@ -17,3 +17,23 @@ def plot_df(df,col,cat=None,linestyle=None,marker=None):
         for opt in options:
             df[col][df[cat]==opt].plot(label=opt,linestyle=linestyle,marker=marker)
             plt.legend()
+            
+def plot_df_multiple(df,cols,cat=None,linestyle=None,marker=None,alpha=None):
+    '''df must be a pandas DataFrame, 
+    cols is a list of columns of df
+    cat is a column of categorical variables
+    TO-DO: pass *args and **kwargs to the plot function
+    '''
+    if len(set(cols)-set(df.columns))>0:
+        print 1
+        return
+    if cat is None:
+        df[cols].plot()
+    else:
+        options=df[cat].unique()
+        for col in cols:
+            for opt in options:
+                df[col][df[cat]==opt].plot(label=opt,linestyle=linestyle,marker=marker,alpha=alpha)
+                plt.legend()
+            plt.show()
+
