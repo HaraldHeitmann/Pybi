e6108a62c03524e1eb54ff604719267908c4f040
Spencer Kee <spencerkee7@gmail.com>
2017-10-31 11:26:38 -0400

    Fix PEP8 issues
    Sometimes line length guidelines cannot be followed in main.py

--- dirty/utilities.py
+++ dirty/utilities.py
@@ -2,8 +2,8 @@ import pandas as pd
 from matplotlib import pyplos as plt
 
 
-def plot_df(df,col,cat=None,linestyle=None,marker=None):
-    '''df must be a pandas DataFrame, 
+def plot_df(df, col, cat=None, linestyle=None, marker=None):
+    '''df must be a pandas DataFrame,
     col and cat should be in df.columns and
     cat is a categorical variable
     TO-DO: pass *args and **kwargs to the plot function
@@ -13,26 +13,42 @@ def plot_df(df,col,cat=None,linestyle=None,marker=None):
     if cat is None:
         df[col].plot()
     else:
-        options=df[cat].unique()
+        options = df[cat].unique()
         for opt in options:
-            df[col][df[cat]==opt].plot(label=opt,linestyle=linestyle,marker=marker)
+            df[col][
+                df[cat] == opt].plot(
+                label=opt,
+                linestyle=linestyle,
+                marker=marker)
             plt.legend()
-            
-def plot_df_multiple(df,cols,cat=None,linestyle=None,marker=None,alpha=None):
-    '''df must be a pandas DataFrame, 
+
+
+def plot_df_multiple(
+        df,
+        cols,
+        cat=None,
+        linestyle=None,
+        marker=None,
+        alpha=None):
+    '''df must be a pandas DataFrame,
     cols is a list of columns of df
     cat is a column of categorical variables
     TO-DO: pass *args and **kwargs to the plot function
     '''
-    if len(set(cols)-set(df.columns))>0:
+    if len(set(cols) - set(df.columns)) > 0:
         print 1
         return
     if cat is None:
         df[cols].plot()
     else:
-        options=df[cat].unique()
+        options = df[cat].unique()
         for col in cols:
             for opt in options:
-                df[col][df[cat]==opt].plot(label=opt,linestyle=linestyle,marker=marker,alpha=alpha)
+                df[col][
+                    df[cat] == opt].plot(
+                    label=opt,
+                    linestyle=linestyle,
+                    marker=marker,
+                    alpha=alpha)
                 plt.legend()
             plt.show()
